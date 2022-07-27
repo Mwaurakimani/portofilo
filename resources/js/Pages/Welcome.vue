44a334ebc69c187e58db08ee11d6ea315b03b331
MwauraKimani <kimmwaus@gmail.com>
2022-07-27 15:53:42 +0300

    Testing update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,7 +2,7 @@
     <div class="banner-image">
         <div class="cover">
             <h1>Welcome</h1>
-            <p>Site will be launching soon!</p>
+            <p>This is the development server</p>
 
             <div class="timer">
                 <div class="timer-digit">
